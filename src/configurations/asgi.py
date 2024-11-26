a60c4139c0dc0fe9da587ab7762e7049467366d9
msrouji <msrouji@biru.sh>
2024-11-26 20:13:48 +0100

    fix: asgi conf wasn't loaded properly

--- src/configurations/asgi.py
+++ src/configurations/asgi.py
@@ -30,12 +30,11 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from events.routing import websocket_urlpatterns
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "configurations.settings")
 
 django_asgi_app = get_asgi_application()
 
+from events.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
