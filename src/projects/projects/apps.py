8b8ad159f0e485b11ad7d18afbeae64615fe1757
Julie Rymer <jrymer@biru.sh>
2025-04-29 17:12:36 +0200

    build: use post_migrate signal instead of data migration to be resilient to flush

---
+++ src/projects/projects/apps.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2024 BIRU
+#
+# This file is part of Tenzu.
+#
+# Tenzu is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# You can contact BIRU at ask@biru.sh
+
+from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+class ProjectsConfig(AppConfig):
+    name = "projects.projects"
+    label = "projects"
+    verbose_name = "Projects"
+
+    def ready(self):
+        from . import signals  # noqa
+        from .signals import fill_project_template
+
+        post_migrate.connect(
+            fill_project_template, dispatch_uid="fill_project_template", sender=self
+        )
