1ddaa3d4378d40f0e57a7b4ff8ee4d9423f2c9f8
Julie Rymer <jrymer@biru.sh>
2026-02-07 00:34:52 +0100

    perf: add async authenticate

--- src/auth/backends.py
+++ src/auth/backends.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2024-2025 BIRU
+# Copyright (C) 2024-2026 BIRU
 #
 # This file is part of Tenzu.
 #
@@ -39,6 +39,25 @@ class EmailOrUsernameModelBackend(ModelBackend):
             User().set_password(password)
         else:
             if user.check_password(password) and self.user_can_authenticate(user):
+                return user
+
+    async def aauthenticate(self, request, username=None, password=None, **kwargs):
+        if username is None:
+            username = kwargs.get(User.USERNAME_FIELD)
+        if username is None or password is None:
+            return
+        try:
+            user = await User._default_manager.aget(
+                Q(**{User.USERNAME_FIELD: username}) | Q(**{User.EMAIL_FIELD: username})
+            )
+        except User.DoesNotExist:
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a nonexistent user (#20760).
+            User().set_password(password)
+        else:
+            if await user.acheck_password(password) and self.user_can_authenticate(
+                user
+            ):
                 return user
 
 
